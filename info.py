3fd3d88e1280fb1ce43bafe81965f60ed44c97df
Suryansh vikram singh <69345576+suryanshvikramsingh@users.noreply.github.com>
2023-04-13 13:41:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,7 +76,7 @@ TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
-DATABASE_URI = environ.get('mongodb+srv://SVSFILTERBOT:SVSFILTERBOT@cluster0.1thbtci.mongodb.net/?retryWrites=true&w=majority', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Professorbot:Professorbot@cluster0.uepjgag.mongodb.net/Cluster0?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
